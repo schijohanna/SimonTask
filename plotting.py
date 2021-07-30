130b87260551e7aa8e5f34811ddcca2ab97f5855
Georg Fasching <georg.fasching5@gmail.com>
2021-07-30 16:43:41 +0200

    congruency updated

--- plotting.py
+++ plotting.py
@@ -7,21 +7,36 @@ data = pd.read_csv("data.csv", header=1)
 #renaming values
 data.loc[:,"Position"] = data["Position"].replace({
     0:"middle",
-    300:"right",
-    -300:"left"
+    350:"right",
+    -350:"left"
 })
 
 data.loc[:,"Colour"] = data["Colour"].replace({
-    "Colour(red=200, green=200, blue=200)":"grey",
+    "Colour(red=200, green=200, blue=200)":"white",
     "Colour(red=0, green=255, blue=0)":"green",
     "Colour(red=0, green=0, blue=255)":"blue"
 })
 
 data.loc[:,"Button"] = data["Button"].replace({
-    98:"B",
-    102:"F",
-    106:"J"
+    98:"white",
+    100:"green",
+    107:"blue"
 })
 
+
+def check_Congruency(row):
+    but = row["Button"]
+    pos = row["Position"]
+    if but == "white":
+        return "no"
+    if (but == "green" and pos== "left") or (but== "blue" and pos== "right"):
+        return "congruent"
+    if (but == "green" and pos== "right") or (but== "blue" and pos== "left"):
+        return "incongruent"
+
+
+data["Correctness"] = data["Colour"] == data["Button"]
+data = data.loc[data['Correctness'] == True]
+data["Congruency"] = data.apply(lambda row: check_Congruency(row), axis=1)
 
 print(data)
